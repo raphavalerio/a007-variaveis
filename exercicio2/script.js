034cfd12101f94bea50ec25c507b2f5cc4a28dc3
Raphael Valerio <raphaelsvalerio@hotmail.com>
2022-07-18 23:52:50 -0300

    Exercicios Fixação A007

---
+++ exercicio2/script.js
@@ -0,0 +1,11 @@
+const perguntaA = ("Você gosta de futebol?")
+const perguntaB = ("Pelé é melhor que o Maradona?")
+const perguntaC = ("O Palmeiras tem Mundial?")
+
+const respostaA = prompt(perguntaA)
+const respostaB = prompt(perguntaB)
+const respostaC = prompt(perguntaC)
+
+console.log(perguntaA,"-",respostaA)
+console.log(perguntaB,"-",respostaB)
+console.log(perguntaC,"-",respostaC)
---
+++ exercicio3/script.js
@@ -0,0 +1,10 @@
+let a = prompt("Digite um valor para a")
+let b = prompt("Digite um valor para b")
+let c;
+
+c=a
+a=b
+b=c
+
+console.log("O novo valor de a é",a)
+console.log("O novo valor de b é",b)
